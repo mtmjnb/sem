44fee4b70936a06d4b1f20c422345fda3e1b7824
mtmjnb <fakemtmjnb@gmail.com>
2024-11-11 15:16:24 +0000

    Add local connections

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -13,8 +13,11 @@ public class App {
         // Create new Application
         App app = new App();
 
-        // Connect to database
-        app.connect();
+        if (args.length < 1) {
+            app.connect("localhost:33060", 10000);
+        } else {
+            app.connect(args[0], Integer.parseInt(args[1]));
+        }
 
         ArrayList<Employee> employees = app.getSalariesByDepartment(app.getDepartment("Sales"));
         app.printSalaries(employees);
@@ -26,7 +29,7 @@ public class App {
     /**
      * Connect to the MySQL database.
      */
-    public void connect() {
+    public void connect(String location, int delay) {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -36,18 +39,26 @@ public class App {
         }
 
         int retries = 10;
+        boolean shouldWait = false;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
+                if (shouldWait) {
+                    // Wait a bit for db to start
+                    Thread.sleep(delay);
+                }
+
                 // Connect to database
-                connection = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                connection = DriverManager.getConnection("jdbc:mysql://" + location +
+                        "/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
+
+                // Let's wait before attempting to reconnect
+                shouldWait = true;
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
